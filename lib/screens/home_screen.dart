1abf4dfa327962a6aec6f21c8a5223e27dda9432
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-19 15:17:45 +0530

    Nav Bar complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,103 +29,122 @@ class HomeScreen extends StatelessWidget {
             topRight: Radius.circular(30),
           ),
         ),
-        height: 180,
+        height: 170,
         child: Column(
           children: [
-            Container(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: 25,
-                      backgroundColor: Colors.white,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 24,
+                      backgroundImage: AssetImage('assets/cd_bg.jpeg'),
                       child: CircleAvatar(
-                        radius: 24,
-                        backgroundImage: AssetImage('assets/cd_bg.jpeg'),
+                        radius: 8,
+                        backgroundColor: Colors.white,
                         child: CircleAvatar(
-                          radius: 8,
-                          backgroundColor: Colors.white,
-                          child: CircleAvatar(
-                            radius: 7,
-                            backgroundColor: Colors.black,
-                          ),
+                          radius: 7,
+                          backgroundColor: Colors.black,
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    const SizedBox(
-                      height: 50,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            'Calm Down',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          'Calm Down',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
                           ),
-                          Text(
-                            'Remu, Salena Gomez',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                            ),
+                        ),
+                        Text(
+                          'Remu, Salena Gomez',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
                           ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 80,
-                    ),
-                    const SizedBox(
-                      height: 50,
-                      child: Center(
-                        child: Icon(
-                          Icons.favorite_border,
-                          color: Colors.white,
-                          size: 25,
                         ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 80,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                    child: Center(
+                      child: Icon(
+                        Icons.favorite_border,
+                        color: Colors.white,
+                        size: 25,
                       ),
                     ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    SizedBox(
-                      height: 45,
-                      width: 45,
-                      child: FloatingActionButton(
-                        backgroundColor: const Color(0xff424243),
-                        onPressed: () => print('Pressed'),
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        child: const Icon(
-                          Icons.pause_rounded,
-                          color: Colors.white,
-                        ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  SizedBox(
+                    height: 45,
+                    width: 45,
+                    child: FloatingActionButton(
+                      backgroundColor: const Color(0xff424243),
+                      onPressed: () => print('Pressed'),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      child: const Icon(
+                        Icons.pause_rounded,
+                        color: Colors.white,
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
               ),
             ),
             Container(
-              height: 90,
+              height: 80,
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
                   topRight: Radius.circular(30),
                 ),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.home_filled,
+                    size: 25,
+                  ),
+                  Icon(
+                    Icons.search_rounded,
+                    size: 25,
+                  ),
+                  Icon(
+                    Icons.library_music_rounded,
+                    size: 25,
+                  ),
+                  Icon(
+                    Icons.favorite,
+                    size: 25,
+                  ),
+                ],
               ),
             )
           ],
