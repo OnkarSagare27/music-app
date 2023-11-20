d08dc92034e681e7070f675dd18d84d3567b3f2b
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-20 23:59:15 +0530

    Now Playing Screen

--- lib/screens/now_playig.dart
+++ lib/screens/now_playig.dart
@@ -5,6 +5,8 @@ class NowPlayingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
   }
 }
